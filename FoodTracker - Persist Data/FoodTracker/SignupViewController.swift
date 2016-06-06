2eca1ee8ee09922c071172af3a561e00f2200c6b
Zach Smoroden <zach@smoroden.com>
2016-06-06 11:41:08 -0700

    added login storyboard. started on login logic

---
+++ FoodTracker - Persist Data/FoodTracker/SignupViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignupViewController.swift
+//  FoodTracker
+//
+//  Created by Zach Smoroden on 2016-06-06.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBOutlet weak var signupButton: UIButton!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
